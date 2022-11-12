d8187d82ccdabd3c6746c39d781361adc099764e
Frank Nguyen <frank@ultra.me>
2022-11-12 15:50:48 -0800

    added sample questions and writeToFile function to test functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        mesage: "What is the title of your project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is the motivation for building this project?"
+    }
+];
 
+inquirer.prompt(questions).then((answers) => {
+    const fileName = `README_TEST.md`
+    // const answers = 
+    writeToFile(fileName, (JSON.stringify(answers, null, '  ')));
+});
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+let writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log('Success!'));
+}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
